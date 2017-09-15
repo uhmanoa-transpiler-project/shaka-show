8ba66fb62893362df79494b8967c2a2166e80480
1010jms <1010jms@gmail.com>
2017-09-15 05:59:06 -1000

    Updated print aesthetics

--- shaka_show/showutil.py
+++ shaka_show/showutil.py
@@ -5,17 +5,14 @@ parser.add_argument("file", help="reads in the source file name");
 args = parser.parse_args()
 
 if args.port:
+	print("\n")
+	print("PORT: " + args.port + "\n")
 
-	print("PORT: " + args.port)
-
-
-print("Source File: " + args.file)
+print("Source File: " + args.file + "\n")
 
 source = open(args.file, "r")
 for line in source:
 	print(line)
-
-
 
 source.close()
 
