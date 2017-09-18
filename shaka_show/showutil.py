6a765fb1f416558c58fad5b63244d23f1cf7eda6
1010jms <1010jms@gmail.com>
2017-09-18 09:15:03 -1000

    Placed parser code into parseSource() function

--- shaka_show/showutil.py
+++ shaka_show/showutil.py
@@ -1,18 +1,38 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--port", help="assigns the port number");
-parser.add_argument("file", help="reads in the source file name");
-args = parser.parse_args()
 
-if args.port:
-	print("\n")
-	print("PORT: " + args.port + "\n")
+def parseSource ():
+	#parse the input
+	parser = argparse.ArgumentParser()
 
-print("Source File: " + args.file + "\n")
+	#optionally check for a port number input; if no input, default port number to 8888
+	parser.add_argument("-p", "--port", help="assigns the port number", default="8888");
 
-source = open(args.file, "r")
-for line in source:
+	#Check for source file input; if no input, return error
+	parser.add_argument("file", help="reads in the source file name");
+
+	#Assign parsed components to 'args'
+	args = parser.parse_args()
+
+	# Open source file and append each line of code to a list
+	source = open(args.file, "r")
+	sourceList = []
+	for line in source:
+		sourceList.append(line)
+
+	# Create a dictionary containing the 'port number,' the 'file name,' and the 'source code' of the source file
+	dict = {'PORT': args.port, 'FileName': args.file, 'Source': sourceList }
+
+	#Close the source file
+	source.close()
+
+	#return the dictionary value
+	return dict
+
+newDict = parseSource()
+
+print("PORT: " + newDict['PORT'] + "\n")
+print("FileName: " + newDict['FileName'] + "\n")
+
+for line in newDict['Source']:
 	print(line)
-
-source.close()
 
