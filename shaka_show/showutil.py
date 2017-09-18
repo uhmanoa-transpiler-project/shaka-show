2178f69443d32afc76ea0d0c2a9867ec202c7d99
1010jms <1010jms@gmail.com>
2017-09-18 09:25:02 -1000

    Renamed the function and the dict & added function description

--- shaka_show/showutil.py
+++ shaka_show/showutil.py
@@ -1,6 +1,14 @@
 import argparse
 
-def parseSource ():
+def parseArgs ():
+	
+	"""
+	Function takes in arguments from a command line.
+	If the argument is preceded by '-p' or '--port', the value for the source file's port number is changed from '8888' to the argument value.
+	One of the input arguments is the source file; if the argument is empty or if the source file does not exist, return error.
+	Function returns a dictionary of the port number, the file name, and the source code of the source file.
+	"""
+
 	#parse the input
 	parser = argparse.ArgumentParser()
 
@@ -20,19 +28,10 @@ def parseSource ():
 		sourceList.append(line)
 
 	# Create a dictionary containing the 'port number,' the 'file name,' and the 'source code' of the source file
-	dict = {'PORT': args.port, 'FileName': args.file, 'Source': sourceList }
+	newDict = {'PORT': args.port, 'FileName': args.file, 'Source': sourceList }
 
 	#Close the source file
 	source.close()
 
 	#return the dictionary value
-	return dict
-
-newDict = parseSource()
-
-print("PORT: " + newDict['PORT'] + "\n")
-print("FileName: " + newDict['FileName'] + "\n")
-
-for line in newDict['Source']:
-	print(line)
-
+	return newDict
