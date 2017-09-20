81d46a66a41069b5df26134c0bfb1e815841d34d
beejayines <beejayi@hawaii.edu>
2017-09-20 00:38:21 -1000

    Pub and Sub now works

--- Pub.py
+++ Pub.py
@@ -1,25 +1,20 @@
 #Publisher: Sends out message every few seconds
 
 import zmq
-import sys
 import time
-
-#Create port number for publisher
-port = "8888"
-if len(sys.argv) > 1:
-    port = sys.argv[1]
-    int(port)
-
+from random import randrange
 #Create socket as Publisher
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:%s" % port)
+socket.bind("tcp://*:5556")
 
 #Continuously send out a message every 5 seconds
 while True:
-    print("Sending Message")
-    #message = "Hello World"
+    #print("Sending Message")
+    zipcode = randrange(1, 100000)
+    message = "Hello World!"
     #b = message.encode('utf-8')
-    socket.send(b"Hello World!")
-    print("Message sent")
-    time.sleep(10)
+    #socket.send(b)
+    socket.send_string("%i*%s" % (zipcode, message))
+    #print("Message sent")
+    #time.sleep(10)
