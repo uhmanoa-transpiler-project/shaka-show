3ec76119edeb0ec4e069233ade4ca9d0b0b83b02
1010jms <1010jms@gmail.com>
2017-09-22 06:44:37 -1000

    Created zmqhandlers.py

---
+++ shaka_show/base/zmqhandlers.py
@@ -0,0 +1,39 @@
+#Subscriber: Whenever a message is heard, print message
+
+import zmq
+import sys
+
+from zmq.eventloop import ioloop, zmqstream
+ioloop.install()
+
+#Create socket as Subscriber
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+stream = zmqstream.ZMQStream(socket)
+#stream.on_recv()
+
+#Connect to Publisher and subscribe
+print("Connecting to Pub")
+socket.connect("tcp://localhost:5556")
+
+zip_filter = sys.argv[1] if len(sys.argv) > 1 else "10001"
+if isinstance(zip_filter, bytes):
+    zip_filter = zip_filter.decode('ascii')
+socket.setsockopt_string(zmq.SUBSCRIBE, zip_filter)
+print("Connected to Pub")
+
+#print message
+while True:
+    print("Waiting for Message")
+    string = socket.recv_string()
+    zipcode, message = string.split('*')
+    print("Message received")
+    print(message)
+
+
+
+
+
+
+
