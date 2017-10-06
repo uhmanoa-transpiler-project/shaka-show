78c8a9c7f13ed1bee7fd0be71ef92d0000c0a8f4
1010jms <1010jms@gmail.com>
2017-10-05 21:26:11 -1000

    Turned Sub Socket into a Function

--- shaka_show/base/zmqhandlers.py
+++ shaka_show/base/zmqhandlers.py
@@ -8,27 +8,28 @@ from zmq.eventloop import ioloop, zmqstream
 ioloop.install()
 
 
+List = []
+
+
 def getcommand(msg):
     command = msg[0].decode('UTF-8')
     print("Received control command: %s" % command)
     if command == "Exit":
         print("Received exit command, client will stop receiving messages")
-        should_continue = False
         ioloop.IOLoop.instance().stop()
 
+    else:
+        List.append(command)
 
-def process_message(msg):
-    print("Processing ... %s" % msg)
 
-
-port_sub = "5558"
-context = zmq.Context()
-socket_sub = context.socket(zmq.SUB)
-socket_sub.connect("tcp://localhost:%s" % port_sub)
-socket_sub.setsockopt_string(zmq.SUBSCRIBE, "")
-stream_sub = zmqstream.ZMQStream(socket_sub)
-stream_sub.on_recv(getcommand)
-print("Connected to publisher with port %s" % port_sub)
-ioloop.IOLoop.instance().start()
-print("Worker has stopped processing messages.")
-
+def zmq_sub(port_sub = "8080"):
+    context = zmq.Context()
+    socket_sub = context.socket(zmq.SUB)
+    socket_sub.connect("tcp://localhost:%s" % port_sub)
+    socket_sub.setsockopt_string(zmq.SUBSCRIBE, "")
+    stream_sub = zmqstream.ZMQStream(socket_sub)
+    stream_sub.on_recv(getcommand)
+    print("Connected to publisher with port %s" % port_sub)
+    ioloop.IOLoop.instance().start()
+    print("Worker has stopped processing messages.")
+    return List
