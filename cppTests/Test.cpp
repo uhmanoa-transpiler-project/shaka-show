f9a11c6cb3233f94f213322160a96ee4a406472d
1010jms <1010jms@gmail.com>
2017-10-19 01:18:44 -1000

    stdout saved from Test.cpp to results.txt

--- cppTests/Test.cpp
+++ cppTests/Test.cpp
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
 int main() {
+	int num = 1;
+	int numList[100];
+	int count = 0;
+
+	//Continue to take in Integers Until -1 is Inputted
+	while(num != -1) {
+		printf("Please Enter a Number (Enter '-1' to Quit): ");
+		scanf("%d", &num);
+		if(num != -1) {
+			numList[count] = num;
+			count++;
+		}
+	}
+
+	/************************************************************/
+	/* Opens/Creates "results.txt" and Records "stdout" Into It */
+	/************************************************************/
+
 	freopen("results.txt", "w", stdout);
-	printf("Hello World! \n");
+
+	/************************************************************/
+	/************************************************************/
+
+	//Output that is recorded into "results.txt"
+	if(count < 5) 
+		printf("Brah... Not Enough Numbers!\n");
+
+	else
+		for(int i = 0; i < count; i++)	printf("%d\n", numList[i]);
+
 	return 0;
 }
