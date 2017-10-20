c46425b46e87b859a292d4569da79cbe979418ab
Paulo Lemus <paulolemus14@gmail.com>
2017-10-19 21:03:58 -1000

    Fixed codemirror issues, working on codemirror ajax

---
+++ shaka_show/static/panels/sourcetracker/js/codemirror.js
@@ -0,0 +1,7 @@
+const codeMirror = CodeMirror(document.getElementById('shaka-codemirror'), {
+  mode: 'scheme',
+  lineNumbers: true,
+  value: '; Scheme test\n; Testing 123\n(define x 1)\n',
+});
+
+export default codeMirror;
--- shaka_show/static/panels/sourcetracker/js/index.js
+++ shaka_show/static/panels/sourcetracker/js/index.js
@@ -2,4 +2,5 @@ console.log('entered codetracker/index.js');
 
 // Set up code tracker
 require('./test.js');
-require('./init-codemirror.js');
+require('./codemirror.js');
+require('./ajax.js');
